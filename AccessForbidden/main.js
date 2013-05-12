5aa26eabfeda113b3513ad7765060bb8f13054ae
iwagaki <iwagaki@users.sourceforge.net>
2013-05-12 19:05:26 +0900

    Supported a white list

--- AccessForbidden/main.js
+++ AccessForbidden/main.js
@@ -8,11 +8,21 @@ chrome.storage.local.get([
     var arrayOfLines = items['urls'].split('\n');
     for (var i = 0; i < arrayOfLines.length; i++) {
         var regex = trim(arrayOfLines[i]);
+        var isWhiteList = false;
         if (regex != '') {
+            if (regex.substring(0, 1) == '!') {
+                regex = regex.substring(1, regex.length);
+                isWhiteList = true;
+            }
             var re = new RegExp(regex);
-            if(re.test(document.URL)) {
-                window.stop();
-                break;
+
+            if (re.test(document.URL)) {
+                if (isWhiteList) {
+                    break;
+                } else { 
+                    window.stop();
+                    break;
+                }
             }
         }
     }
